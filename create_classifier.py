975fa508648bb0b123e93cdb81a9a25199dc989e
Aman Arya <aman.arya524@gmail.com>
2017-06-15 15:59:06 -0400

    First commit

---
+++ create_classifier.py
@@ -0,0 +1,16 @@
+import time
+from dipy.tracking.local import ThresholdTissueClassifier
+import csd_peaks
+
+def create_classifier(peaks):
+    # Restricts the fiber tracking to the restricted diffusion
+    start = time.time()
+    classifier = ThresholdTissueClassifier(peaks.gfa, .1)
+    end = time.time()
+    print('Created Tissue classifer: ' + str((end - start)))
+    return classifier
+
+def classifier():
+    return create_classifier(csd_peaks.make_peaks())
+
+
---
+++ create_mask.py
@@ -0,0 +1,23 @@
+import time
+import numpy as np
+import load_file
+
+def create_wm_mask(aparc):
+    # Create the white matter mask
+    start = time.time()
+    wm_regions = [2, 41, 16, 17, 28, 60, 51, 53, 12, 52, 12, 52, 13, 18,
+                  54, 50, 11, 251, 252, 253, 254, 255, 10, 49, 46, 7]
+
+    wm_mask = np.zeros(aparc.shape)
+    for l in wm_regions:
+        wm_mask[aparc == l] = 1
+    end = time.time()
+    print("Created wm mask: " + str((end - start)))
+    return wm_mask
+
+def make_mask():
+    d = load_file.load_files()
+    return create_wm_mask(load_file.load_aparc(d.data_fs_seg))
+
+
+
