d87d898b262410a2ec9eaae0e232f9d5952fac1d
Aman Arya <aman.arya524@gmail.com>
2017-06-18 16:35:28 -0400

    Deleted complete_tracking files and updated main.py

--- main.py
+++ main.py
@@ -1,60 +1,121 @@
 import time
-from dipy.tracking.local import LocalTracking
-import csd_peaks, create_classifier, create_seeds, load_file, prob_dg
-from nibabel.streamlines import Tractogram, save
-from dipy.viz import fvtk
+import numpy as np
+import nibabel as nib
+import json
+from dipy.core.gradients import gradient_table
+from dipy.viz import fvtk, actor, window
 from dipy.viz.colormap import line_colors
+from dipy.tracking import utils
+from dipy.io.gradients import read_bvals_bvecs
+from dipy.reconst.shm import CsaOdfModel
+from dipy.data import default_sphere
+from dipy.direction import peaks_from_model
+from dipy.tracking.local import ThresholdTissueClassifier
+from dipy.tracking.local import LocalTracking
+from nibabel.streamlines import Tractogram, save
+from dipy.reconst.csdeconv import (ConstrainedSphericalDeconvModel,
+                                   auto_response)
+from dipy.direction import ProbabilisticDirectionGetter
 
-
-def compute_streamlines(peaks, classifier, seeds, affine):
-    # Initialization of LocalTracking. The computation happens in the next step.
+def main():
     start = time.time()
-    streamlines = LocalTracking(peaks, classifier, seeds, affine, step_size=.5)
+
+    with open('config.json') as config_json:
+        config = json.load(config_json)
+    
+    # Load the data
+    dmri_image = nib.load(config['data_file'])
+    dmri = dmri_image.get_data()
+    affine = dmri_image.affine
+    aparc_im = nib.load(config['data_fs_seg'])
+    aparc = aparc_im.get_data()
+    end = time.time()
+    print('Loaded Files: ' + str((end - start)))
+
+    # Create the white matter and callosal masks
+    start = time.time()
+    wm_regions = [2, 41, 16, 17, 28, 60, 51, 53, 12, 52, 12, 52, 13, 18,
+                  54, 50, 11, 251, 252, 253, 254, 255, 10, 49, 46, 7]
+
+    wm_mask = np.zeros(aparc.shape)
+    for l in wm_regions:
+        wm_mask[aparc == l] = 1
+
+    callosal_regions = [255, 254, 253]
+    callosum = np.zeros(aparc.shape)
+    for c in callosal_regions:
+        callosum[aparc == c] = 1
+
+    # Create the gradient table from the bvals and bvecs
+
+    bvals, bvecs = read_bvals_bvecs(config['data_bval'], config['data_bvec'])
+
+    gtab = gradient_table(bvals, bvecs, b0_threshold=100)
+    end = time.time()
+    print('Created Gradient Table: ' + str((end - start)))
+
+    ##The probabilistic model##
+
+    # Use the Constant Solid Angle (CSA) to find the Orientation Dist. Function
+    # Helps orient the wm tracts
+    start = time.time()
+    csa_model = CsaOdfModel(gtab, sh_order=6)
+    csa_peaks = peaks_from_model(csa_model, dmri, default_sphere,
+                                 relative_peak_threshold=.8,
+                                 min_separation_angle=45,
+                                 mask=wm_mask)
+    print('Creating CSA Model: ' + str(time.time() - start))
+
+    # Begins the seed in the wm tracts
+    seeds = utils.seeds_from_mask(wm_mask, density=[1, 1, 1], affine=affine)
+    print('Created White Matter seeds: ' + str(time.time() - start))
+
+    # Create a CSD model to measure Fiber Orientation Dist
+    print('Begin the probabilistic model')
+
+    response, ratio = auto_response(gtab, dmri, roi_radius=10, fa_thr=0.7)
+    csd_model = ConstrainedSphericalDeconvModel(gtab, response, sh_order=6)
+    csd_fit = csd_model.fit(dmri, mask=wm_mask)
+    print ('Created the CSD model: ' + str(time.time() - start))
+
+    # Set the Direction Getter to randomly choose directions
+
+    prob_dg = ProbabilisticDirectionGetter.from_shcoeff(csd_fit.shm_coeff,
+                                                        max_angle=30.,
+                                                        sphere=default_sphere)
+    print('Created the Direction Getter: ' + str(time.time() - start))
+
+    # Restrict the white matter tracking
+    classifier = ThresholdTissueClassifier(csa_peaks.gfa, .25)
+
+    print('Created the Tissue Classifier: ' + str(time.time() - start))
+
+    # Create the probabilistic model
+    streamlines = LocalTracking(prob_dg, tissue_classifier=classifier, seeds=seeds, affine=affine,
+                                step_size=.5, max_cross=1)
+    print('Created the probabilistic model: ' + str(time.time() - start))
 
     # Compute streamlines and store as a list.
     streamlines = list(streamlines)
-    print("Number of streamlines " + str(len(streamlines)))
-    end = time.time()
-    print("Computed streamlines " + str((end - start)))
-    return streamlines
-
-def create_trk(streamlines, affine, name):
-    start = time.time()
+    print('Computed streamlines: ' + str(time.time() - start))
+    
+    # Create a tractogram from the streamlines and save it 
     tractogram = Tractogram(streamlines, affine_to_rasmm=affine)
-    save(tractogram, name + '.trk')
+    save(tractogram, 'csa_prob.trk')
     end = time.time()
     print("Created the trk file: " + str((end - start)))
 
-def make_picture(name, streamlines):
     # Prepare the display objects.
-    start = time.time()
     print("Making pretty pictures")
     if fvtk.have_vtk:
         streamlines_actor = fvtk.line(streamlines, line_colors(streamlines))
-
         # Create the 3d display.
         r = fvtk.ren()
         fvtk.add(r, streamlines_actor)
-
         # Save still images for this static example.
-        fvtk.record(r, n_frames=1, out_path=name + '.png',
+        fvtk.record(r, n_frames=1, out_path='probabilistic.png',
                     size=(800, 800))
-    end = time.time()
-    print ('Made pretty pictures: ' + str((end - start)))
 
+    print ('Made pretty pictures')
 
-def streamlines():
-    probdg = prob_dg.make_probdg
-    classifier = create_classifier.classifier()
-    seeds = create_seeds.seeds()
-    d = load_file.load_files()
-    affine = load_file.load_affine(d.data_file)
-    streamlines = compute_streamlines(probdg, classifier, seeds, affine)
-    create_trk(streamlines=streamlines, affine=affine, name='prob')
-    make_picture('prob', streamlines=streamlines)
-
-streamlines()
-print('Hooray')
-
-
-
+main()
